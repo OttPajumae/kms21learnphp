dec2b16d6d41e394b4bf8fb27a99164aa1b65209
Ott Pajumäee <ottpajumae@gmail.com>
2022-09-30 11:40:51 +0300

    Add index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$var = new \stdClass();
+var_dump($var);
+
+
+/*$name = 'Ott';
+$string = "Sup $name"; /*See on tähtede jaoks. Kui kirjutada siis see annab ka tähtede arvu*/
+/*var_dump($string); */
+/*$num = 1;
+var_dump($num);*/
+
+
+/*echo "hello world!";*/
