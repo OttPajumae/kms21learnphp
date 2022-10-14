54e5b5e40bcdb002fe0a63ca07711a8e0db386c6
Ott Pajumäe <KMS-21-809@tptlive.ee>
2022-10-14 09:22:46 +0300

    interface example

--- index.php
+++ index.php
@@ -1,7 +1,45 @@
 <?php
 
-$var = new \stdClass();
-var_dump($var);
+
+class Job {
+    private $logger;
+
+    public function __construct(ConsoleLogger $logger){
+        $this->logger=$logger;
+    }
+    
+    public function work(){
+        for($i=1;$i<10; $i++){
+
+            $this->logger->log($i);
+        }
+    }
+}
+
+class ConsoleLogger{
+    public function log($message){
+        echo $message, "\n";
+    }
+}
+
+interface Logger{
+    public function log($message);
+}
+///////////////////////
+
+class NothingLogger{
+    public function log($message){
+
+    }
+}
+
+
+$consoleLogger=new ConsoleLogger();
+$nothingLogger=new NothingLogger();
+$job = new Job($consoleLogger);
+$job->work();
+// $var = new \stdClass();
+// var_dump($var);
 
 
 /*$name = 'Ott';
